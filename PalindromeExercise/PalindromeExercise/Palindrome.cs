a459389413a55a4c9563919ce00f6eb89d0bcd91
Blake Brashear <bbrashear94@gmail.com>
2020-04-22 13:59:02 -0400

    Completed instructions for the exercise

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            bool isPalindrome;
+            var arr = word.ToCharArray();
+            Array.Reverse(arr);
+            string reverseWord = new string(arr);
+            if (word == reverseWord)
+            {
+                isPalindrome = true;
+            }
+            else
+            {
+                isPalindrome = false;
+            }
+            return isPalindrome;
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("civic", true)]
+        [InlineData("hello", false)]
+        [InlineData("world", false)]
+        [InlineData("great", false)]
+         
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var sut = new Palindrome();
 
+            //Act
+            var actual = sut.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
